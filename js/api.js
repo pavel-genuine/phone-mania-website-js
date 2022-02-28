2bbf363a02a5cda24a97e34734c5eaa6747726a2
pavel.genuine <pavel.play@gmail.com>
2022-03-01 03:39:24 +0600

    responsive

--- js/api.js
+++ js/api.js
@@ -7,6 +7,7 @@ const searchData =()=>{
     document.getElementById('cards').innerHTML=``
     document.getElementById('detail').innerHTML=``
     document.getElementById('counts').innerHTML=``
+    document.getElementById('result-count').innerHTML=``
 
 
     const searchText = document.getElementById('search').value
@@ -56,12 +57,14 @@ const displayData=phones=>{
 
         // geting data from api dynamically
         card.innerHTML=`
-        <div class="card w-75">
+        <div class='d-flex justify-content-center align-items-center'>
+        <div class="card w-75 shadow">
           <img src="${phone.image}" class=" w-75 mx-auto mt-4 card-img-top" alt="no results">
           <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">${phone.brand}</p>
             <button onclick="displayDetail('${phone.slug}')" class='px-4 py-1 fw-bold bg-danger rounded border-0 text-white'>Detail</button>
+          </div>
           </div>
        </div>`
 
@@ -87,26 +90,46 @@ const showDetail = data =>{
 const detailDiv = document.getElementById('detail')
 
 // getting data from api dynamically 
-detailDiv.innerHTML=`<div class="card w-25 mx-auto mb-4">
-<img src="${data.image}" class=" w-75 mx-auto mt-4 card-img-top" alt="no results">
+detailDiv.innerHTML=`
+
+<div class="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center align-items-center  my-4">
+         <div class='col'>
+<div class="card w-75 mx-auto mb-4 shadow ">
+<img src="${data.image}" class=" w-75  mx-auto mt-4 card-img-top" alt="no results">
 <div class="card-body">
   <h5 class="card-title"> <span class='fw-bold'>Name: </span>${data.name}</h5>
   <p class="card-text"><span class='fw-bold'>Brand: </span>${data.brand}</p>
+  <p class="card-text"><span class='fw-bold'>Main Features : </span> </p>
+
   <p class="card-text"><span class='fw-bold'>Chip Set: </span>${data.mainFeatures.chipSet}</p>
   <p class="card-text"><span class='fw-bold'>Display Size : </span>${data.mainFeatures.displaySize}</p>
   <p class="card-text"> <span class='fw-bold'>Memory: </span>${data.mainFeatures.memory}</p>
   <p id='sensors' class="card-text"><span class='fw-bold'>Sensors: </span></p>
   <p class="card-text"><span class='fw-bold'>Storage: </span>${data.mainFeatures.storage}</p>
+  <p class="card-text"><span class='fw-bold'>Other: </span> Bluetooth: ${data.others?.Bluetooth ? data.others?.Bluetooth :'none'},  
+  GPS: ${data.others?.GPS ? data.others?.GPS :'none'}, NFC: ${data.others?.NFC ? data.others?.NFC : 'none'}, Radio: ${data.others?.Radio ? data.others?.Radio : 'none'}, 
+  USB: ${data.others?.USB ? data.others?.USB : 'none' }, WLAN: ${data.others?.WLAN ? data.others?.WLAN : 'none '}</p>
+  
   <p class="card-text"><span class='fw-bold'>Release: </span>${data.releaseDate ? data.releaseDate : 'No Release Date Available'}</p>
 
   </div>
+  </div>
+  </div>
   </div>`
 
+//   others:
+// Bluetooth: "5.0, A2DP, LE"
+// GPS: "Yes, with A-GPS, GLONASS, GALILEO, BDS, QZSS"
+// NFC: "Yes"
+// Radio: "No"
+// USB: "Lightning, USB 2.0"
+// WLAN: "Wi-Fi 802.11 a/b/g/
+//   getting sensors info 
   const sensorsId = document.getElementById('sensors')
 
   data.mainFeatures.sensors.forEach(sensor=>{
-      const sensorP = document.createElement('p')
-      sensorP.innerText=sensor
+      const sensorP = document.createElement('span')
+      sensorP.innerHTML=`${sensor}, `
 
       sensorsId.appendChild(sensorP)
   })
